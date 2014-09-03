5876966d54f9926b327ec2f1118f62604fe37c65
Oleksandr <nevkluchat@gmail.com>
2014-09-03 11:56:54 +0300

    Initial Commit

---
+++ MapLocation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapLocation
+//
+//  Created by Administrator on 03.09.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
