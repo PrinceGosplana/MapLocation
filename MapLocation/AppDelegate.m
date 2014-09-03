68c836e82e5952273f3b6710228ba99dc82ea2d3
Oleksandr <nevkluchat@gmail.com>
2014-09-03 15:44:31 +0300

    Add some code from another resource

--- MapLocation/AppDelegate.m
+++ MapLocation/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -16,6 +18,25 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    _viewController = [[ViewController alloc] init];
+    
+    _navigationController = [[UINavigationController alloc] init];
+    _navigationController.viewControllers = [NSArray arrayWithObject:_viewController];
+   
+    
+    [[UINavigationBar appearance] setBarTintColor:UIColorFromRGB(0x067AB5)];
+    NSShadow *shadow = [[NSShadow alloc] init];
+    shadow.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8];
+    shadow.shadowOffset = CGSizeMake(0, 1);
+    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                           [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                                           shadow, NSShadowAttributeName,
+                                                           [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:21.0], NSFontAttributeName, nil]];
+    
+//    [_navigationController setNavigationBarHidden:YES];
+    
+    self.window.rootViewController = _navigationController;
     return YES;
 }
 
---
+++ MapLocation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapLocation
+//
+//  Created by Administrator on 03.09.14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
